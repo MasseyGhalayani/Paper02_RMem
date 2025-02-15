7e69d44b615a51bb888c6a92a13e711431a15596
masseygh <mghaleyani@gmail.com>
2025-02-15 16:18:16 +0100

    [update] install.sh env bash

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ export https_proxy=http://proxy:80
 
 module load python
 module load cuda/12.6.1
-conda create --name RMem python=3.10
+
+conda create --name RMem python=3.10 -y
 source activate RMem
+
 conda install numpy matplotlib scipy scikit-learn tqdm pyyaml pandas
 pip install opencv-python timm gdown
